6e936084245e3c324dc26616da0b0f1e3ee907a3
Alexander Bassov <blackxes.dev@gmail.com>
2025-06-11 19:34:43 +0200

    Removed CMake from deleted directories and added more gitignores

--- scripts/Windows/CleanProject.bat
+++ scripts/Windows/CleanProject.bat
@@ -1,9 +1,27 @@
 cd ..\..\
-rmdir /s /q bin
-rmdir /s /q bin-int
-Get-ChildItem -Recurse | Where{$_.Name -Match "build"} | Remove-Item
-rmdir /s /q **\CMake
-del /s *.sln
-del /s *.vcxproj
-del /s *.vcxproj.filters
-del /s *.vcxproj.user
+
+
+:: Long live chatgpt
+::
+:: === List of directory names to delete ===
+set dirs_to_delete=bin bin-int build
+
+:: Loop through each directory name
+for %%d in (%dirs_to_delete%) do (
+    echo Looking for directories named "%%d"...
+    
+    :: Recursively find and delete each match under current directory
+    for /d /r %%x in (%%d) do (
+        if exist "%%x" (
+            echo Deleting: %%x
+            rd /s /q "%%x"
+        )
+    )
+)
+
+del /s /q *.sln
+del /s /q *.vcxproj
+del /s /q *.vcxproj.filters
+del /s /q *.vcxproj.user
+
+pause
