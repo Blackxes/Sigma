a88b0fb451b6742e68ff26b144cf022d268e7918
Alexander Bassov <blackxes.dev@gmail.com>
2025-06-30 01:31:30 +0200

    SIGMA-12: Formatting

--- Projects/Sandbox/src/SandboxApp.cpp
+++ Projects/Sandbox/src/SandboxApp.cpp
@@ -11,40 +11,40 @@
 class Sandbox : public Sigma::ApplicationBase
 {
 public:
-    Sandbox(const Sigma::ApplicationCreationOptions &options)
-        : Sigma::ApplicationBase(options)
-    {
-        std::cout << "[Sandbox::Constructor]" << std::endl;
+	Sandbox(const Sigma::ApplicationCreationOptions &options)
+		: Sigma::ApplicationBase(options)
+	{
+		std::cout << "[Sandbox::Constructor]" << std::endl;
 
-        this->PushLayer(std::make_shared<ExampleLayer>());
-    }
-    ~Sandbox()
-    {
-        std::cout << "[Sandbox::Destructor]" << std::endl;
-    }
+		this->PushLayer(std::make_shared<ExampleLayer>());
+	}
+	~Sandbox()
+	{
+		std::cout << "[Sandbox::Destructor]" << std::endl;
+	}
 
-    bool OnInit() override
-    {
-        std::cout << "[Sandbox::Init]" << std::endl;
+	bool OnInit() override
+	{
+		std::cout << "[Sandbox::Init]" << std::endl;
 
-        return true;
-    }
+		return true;
+	}
 
-    bool OnEvent() override
-    {
-        std::cout << "[Sandbox::OnEvent]" << std::endl;
+	bool OnEvent() override
+	{
+		std::cout << "[Sandbox::OnEvent]" << std::endl;
 
-        return true;
-    }
+		return true;
+	}
 };
 
 std::shared_ptr<Sigma::ApplicationBase> Sigma::CreateApplication(const Sigma::ApplicationCommandArgs &args)
 {
-    Sigma::ApplicationCreationOptions creationOptions;
-    creationOptions.title = "My very fancy Sandbox application";
-    creationOptions.dimensions = { 600, 300 };
-    creationOptions.fullScreen = false;
-    creationOptions.targetUps = 120;
+	Sigma::ApplicationCreationOptions creationOptions;
+	creationOptions.title = "My very fancy Sandbox application";
+	creationOptions.dimensions = {600, 300};
+	creationOptions.fullScreen = false;
+	creationOptions.targetUps = 120;
 
-    return std::make_shared<Sandbox>(creationOptions);
+	return std::make_shared<Sandbox>(creationOptions);
 }
